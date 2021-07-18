092f361b9193fd99c13138062723d1b69b51c609
kuldeep joshi <joshi123k@gmail.com>
2021-07-18 12:08:11 +0530

    added admin and user components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { UserDashboardComponent } from './pages/user/user-dashboard/user-dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    NavbarComponent
+    NavbarComponent,
+    DashboardComponent,
+    HomeComponent,
+    LoginComponent,
+    SignupComponent,
+    UserDashboardComponent
   ],
   imports: [
     BrowserModule,
